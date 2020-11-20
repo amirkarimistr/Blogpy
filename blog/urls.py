fef7da7990611f0e33bfcb46b707accade7f5d55
amirkarimi39 <amirkarimi3936@gmail.com>
2020-11-20 10:49:38 +0330

    Part 9,add page-contact.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import IndexPage as Index
+from .views import ContactPage as Contact
+
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('contact/', Contact.as_view(), name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,15 @@ class IndexPage(TemplateView):
 
     def get(self, request, **kwargs):
         all_articles = Article.objects.all()[:9]
+        all_promote_articles = Article.objects.filter(promote=True)
 
         context = {
-            'article_data': all_articles
+            'article_data': all_articles,
+            'promote_article_data': all_promote_articles
         }
 
         return render(request, 'blog/index.html', context)
+
+
+class ContactPage(TemplateView):
+    template_name = 'blog/page-contact.html'
